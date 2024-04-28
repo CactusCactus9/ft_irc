bbaff7222f4f407cceb9044da213936ff8f931df
asmae <asmaebelkacem7@gmail.com>
2024-04-28 09:57:30 +0100

    modified the main

--- main.cpp
+++ main.cpp
@@ -24,43 +24,24 @@ int	main(int ac, char **av){
 			std::cerr << "Enter a port and a password" << std::endl;
 			return (1);
 		}
+		//AZMARA
+		std::string	password = av[2];
+		if (password.find_first_of(" \t") != std::string::npos)
+			throw(std::runtime_error("NO WHITE SPACES ALLOWED!"));
 		serv.setPort(strtol(av[1], NULL, 10));
 		serv.setPassword(av[2]);
 		por = serv.getPort();
 		std::string	s = av[1];
 		if (s.find_first_not_of("0123456789") != std::string::npos){
-			std::cerr << "Only numbers in port!" << std::endl;
-			return (1);
+			throw(std::runtime_error("Only numbers in port!"));//AZMARA
 		}
 		pas = serv.getPassword();
 		if (por < 1024 || por > 49151){
-			std::cerr << "Enter a valid port" << std::endl;
-			return (1);
+			throw(std::runtime_error("Enter a valid port"));//AZMARA
 		}
 		if (pas.size() <= 0){
-			std::cout << "Empty Password!" << std::endl;
-			return (1);
+			throw(std::runtime_error("Empty Password!"));//AZMARA
 		}
-			// std::cout << "av[2] :" << av[2] << std::endl;
-			// std::cout << "out " << strtol(pas.c_str(), NULL, 10) << std::endl;
-			
-		
-			// if (atoi(av[2]) < 32 || atoi(av[2]) > 126){
-			// 	std::cout << "Enter a valid password!" << std::endl;
-			// 	return (1);
-			// }
-		
-		// if (sizeof(av[2]) > 0){
-		// 	for (size_t i = 0; i < sizeof(av[2]); ++i){
-		// 		if (!isprint(strtol(av[2], NULL, 10)))
-		// 			{
-		// 			std::cerr << "Enter a valid password" << std::endl;
-		// 			return (1);
-		// 		}
-
-		// 	}
-
-		// }
 		signal(SIGINT, Server::sigHandler);
 		signal(SIGQUIT, Server::sigHandler);
 		serv.launch_server();
